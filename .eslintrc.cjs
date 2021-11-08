97d41d95fa050f9cafdc96f3292cba05d190b406
Alan <alanraison@users.noreply.github.com>
2021-11-08 16:46:06 +0000

    fix eslintrc issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,6 @@ module.exports = {
   parserOptions: {
     project: ['./tsconfig.json', 'packages/**/tsconfig.json'],
     extraFileExtensions: ['.cjs'],
+    ignorePatterns: ['.eslintrc.cjs'],
   },
 };
